e488bc2933040c65a3d54f55b51cc41b2d84dc06
aksenovaleksey <Aksenov_AV@bk.ru>
2025-06-15 10:23:28 +0300

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/Checkstyle/broken.java
@@ -0,0 +1,24 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+
+    public static final String NEW_WALUE = "";
+    private int sizeOfEmpty = 10;
+    public String surname;
+    String name;
+
+    Broken() {
+    }
+
+    void echo() {
+    }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int a, int b, int c, int d, int e, int f, int g) {
+    }
+}
