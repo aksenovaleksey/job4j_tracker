beb39cd0200fb9b743e95acd813474ccd8612e06
aksenovaleksey <Aksenov_AV@bk.ru>
2025-06-20 09:18:57 +0300

    Перегрузка конструктора [#173890 #546487]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -31,19 +31,4 @@ public class Item {
         this.id = id;
         this.name = name;
     }
-
-    public void printInfo() {
-        System.out.println("id: " + id);
-        System.out.println("name: " + name);
-        System.out.println();
-    }
-
-    public static void main(String[] args) {
-        Item item = new Item();
-        item.printInfo();
-        Item item2 = new Item("Товар 1");
-        item2.printInfo();
-        Item item3 = new Item(123, "Товар 2");
-        item3.printInfo();
-    }
 }
