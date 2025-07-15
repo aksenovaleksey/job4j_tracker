a2607b2d58c5372da0cd606cebf6e03517adec70
aksenovaleksey <Aksenov_AV@bk.ru>
2025-07-15 14:21:14 +0300

    3.6. Реализация класса StartUI. Вывод заявок по имени [#500749]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -46,17 +46,32 @@ public class StartUI {
                 Item item = tracker.findById(id);
                 tracker.delete(id);
                 System.out.println(item != null ? "Заявка удалена успешно." : "Ошибка удаления заявки.");
+            } else if (select == 4) {
+                System.out.println("=== Вывод заявки по id ===");
+                System.out.print("Введите id: ");
+                int id = Integer.parseInt(scanner.nextLine());
+                Item item = tracker.findById(id);
+                if (item != null) {
+                    System.out.println(item);
+                } else {
+                    System.out.println("Заявка с введенным id: " + id + " не найдена.");
+                }
+            } else if (select == 5) {
+                System.out.println("=== Вывод заявок по имени ===");
+                System.out.print("Введите имя: ");
+                String name = scanner.nextLine();
+                Item[] items = tracker.findByName(name);
+                if (items.length > 0) {
+                    for (Item item : items) {
+                        System.out.println(item);
+                    }
+                } else {
+                    System.out.println("Заявки с именем: " + name + " не найдены.");
+                }
             } else if (select == 6) {
                 run = false;
-            }
-            System.out.println("=== Вывод заявки по id ===");
-            System.out.print("Введите id: ");
-            int id = Integer.parseInt(scanner.nextLine());
-            Item item = tracker.findById(id);
-            if (item != null) {
-                System.out.println(item);
             } else {
-                System.out.println("Заявка с введенным id: " + id + " не найдена.");
+                System.out.println("Неверный выбор, попробуйте снова.");
             }
         }
     }
