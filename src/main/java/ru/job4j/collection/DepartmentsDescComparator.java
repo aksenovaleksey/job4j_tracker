88db4f560028374cd72d067d419cb17591447c9b
aksenovaleksey <Aksenov_AV@bk.ru>
2026-03-13 12:14:46 +0300

    3. Отсортировать департаменты [#34352 #559951]

--- src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
+++ src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
@@ -7,19 +7,9 @@ public class DepartmentsDescComparator implements Comparator<String> {
     public int compare(String left, String right) {
         String[] leftParts = left.split("/");
         String[] rightParts = right.split("/");
-        int length = Math.min(leftParts.length, rightParts.length);
 
-        for (int i = 0; i < length; i++) {
-            int res;
-            if (i == 0) {
-                res = rightParts[i].compareTo(leftParts[i]);
-            } else {
-                res = leftParts[i].compareTo(rightParts[i]);
-            }
-            if (res != 0) {
-                return res;
-            }
-        }
-        return Integer.compare(leftParts.length, rightParts.length);
+        int result = rightParts[0].compareTo(leftParts[0]);
+
+        return result != 0 ? result : left.compareTo(right);
     }
 }
